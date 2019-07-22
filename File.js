53248c9d48b1b9e23fad2834755c767d96f31032
AnnaMaetz <50204364+AnnaMaetz@users.noreply.github.com>
2019-07-22 13:38:37 +0200

    Update File.js

--- File.js
+++ File.js
@@ -1,8 +1,13 @@
 import Node from './Node';
 
 export default class extends Node {
-  constructor(name) {
+  constructor(name, body) {
     super(name);
     this.type = 'file';
+    this.body = body;
+  }
+  
+  getBody() {
+    return this.body;
   }
 }
